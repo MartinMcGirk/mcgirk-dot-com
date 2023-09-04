35926a0bdf12a1ed18faf16f6319845f4d4df0ab
Martin McGirk <martin@mcgirk.com>
2023-09-04 16:03:32 +1000

    text changes

--- app/components/ContentWithStickyImage.tsx
+++ app/components/ContentWithStickyImage.tsx
@@ -18,10 +18,7 @@ const ContentWithStickyImage = () => {
                 I lead software teams that deliver.
               </h1>
               <p className="mt-6 text-xl leading-8 text-gray-700 dark:text-blue-50">
-                {/* eslint-disable-next-line react/no-unescaped-entities */}
-                Hi, nice to meet you! I'm Martin McGirk. I'm a Software Team
-                Lead in Sydney, Australia, with nearly 15 years experience
-                specialising in web development.
+                {`Hi, nice to meet you! I'm Martin McGirk. I'm a Software Team Lead in Sydney, Australia, with nearly 15 years experience specialising in web development.`}
               </p>
             </div>
           </div>
@@ -31,52 +28,29 @@ const ContentWithStickyImage = () => {
           <div className="lg:pr-4">
             <div className="max-w-xl text-base leading-7 text-gray-700 dark:text-blue-100 lg:max-w-lg">
               <p>
-                People hire me because they have a team of developers working on
-                a business-critical project, and they need someone to help the
-                team deliver. I can help in a bunch of ways, usually by defining
-                solution architectures, helping the team with the implementation
-                details, liaising with stakeholders, running agile ceremonies,
-                and making sure that we are solving the problems that are
-                relevant to the business.
+                {`People hire me because they have a team of developers working on a business-critical project, and they need someone to help the team deliver. I can help in a bunch of ways, usually by defining solution architectures, helping the team with the implementation details, liaising with stakeholders, running agile ceremonies, and making sure that we are solving the problems that are relevant to your business.`}
               </p>
               <p className="mt-4">
-                Most recently I led a team of 8 people at a large construction
-                firm to ship a first-of-its-kind platform for scoping and
-                managing multi-million dollar construction projects.
+                {`Most recently I led a team of 8 people at a large construction firm to ship a first-of-its-kind platform for scoping and managing multi-million dollar construction projects.`}
               </p>
               <ul
                 role="list"
                 className="mt-8 space-y-8 text-gray-600 dark:text-blue-100"
               >
                 <li className="flex gap-x-3">
-                  {/*<CloudArrowUpIcon className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />*/}
                   <span>
                     <strong className="font-semibold text-gray-900 dark:text-blue-400">
                       I bring experience.
                     </strong>{" "}
-                    With nearly 15 years commercial experience of driving web
-                    application development, I like to think I set myself apart
-                    with my ability to focus on how software can solve business
-                    problems, rather than just how it can solve technical ones.
-                    {/* eslint-disable-next-line react/no-unescaped-entities */}
-                    So far I've been doing that across the construction,
-                    finance, education, oil & gas, and retail sectors.
+                    {`With nearly 15 years commercial experience of driving web application development, I like to think I set myself apart with my ability to focus on how software can solve business problems, rather than just how it can solve technical ones. So far I've been doing that across the construction, finance, education, oil & gas, and retail sectors.`}
                   </span>
                 </li>
                 <li className="flex gap-x-3">
-                  {/*<LockClosedIcon className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />*/}
                   <span>
                     <strong className="font-semibold text-gray-900 dark:text-blue-400">
                       I have full-stack experience.
                     </strong>{" "}
-                    Full stack web development is where I started. My degree is
-                    {/* eslint-disable-next-line react/no-unescaped-entities */}
-                    in software engineering and I've been working with code
-                    {/* eslint-disable-next-line react/no-unescaped-entities */}
-                    since 2011. Since then I've done backend, frontend,
-                    databases, (some) devops, and everything in between. I can
-                    bring that holistic, technical view of how a prodcut is
-                    built to help your project succeed.
+                    {`Full stack web development is where I started. My degree is in software engineering and I've been working with code since 2011. Since then I've done backend, frontend, databases, (some) devops, and everything in between. I've been leading teams for the last 6 years, and I can bring that holistic, technical view of how a prodcut is built to help your project succeed.`}
                   </span>
                 </li>
               </ul>
