7b348bbed8c3d63841ca7bf6bfed580cc0a4170f
Martin McGirk <martin@mcgirk.com>
2025-01-10 21:28:10 +1100

    Rotate the camera instead of rotate the model

--- app/graphics/SydneyModelMoving.tsx
+++ app/graphics/SydneyModelMoving.tsx
@@ -2,259 +2,222 @@
 
 "use client";
 
-import React, { useEffect, useRef } from "react";
-import { useAnimations, useGLTF } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import React, { useRef, useEffect, useMemo } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+import * as THREE from 'three';
 
 export function SydneyModelMoving(props: any) {
-  const groupRef = useRef<THREE.Group>(null!);
-
-  const [mouseCoords, setMouseCoords] = React.useState([0, 0]);
-
-  useEffect(() => {
-    if (window) {
-      window.addEventListener("mousemove", (e) => {
-        setMouseCoords([
-          e.clientX / window.innerWidth - 0.5,
-          e.clientY / window.innerHeight - 0.5,
-        ]);
-      });
-    }
-  }, []);
-
-  const clamp = (val: number, min: number, max: number) => {
-    return val > max ? max : val < min ? min : val;
-  };
-
-  useFrame((state, delta, frame) => {
-    if (!groupRef.current) return;
-    const parallaxX = -mouseCoords[0] * 0.5;
-    const newRotationY =
-      groupRef.current.rotation.y +
-      (parallaxX - groupRef.current.rotation.y) * delta;
-    groupRef.current.rotation.y = clamp(newRotationY, -0.25, 0.25);
-  });
-
-  // @ts-ignore
-  const { nodes, materials } = useGLTF("/models/MovingCity/city.gltf");
-
-  const ferryGroup = useRef();
-  const ferry3Group = useRef();
+  const { nodes: cityNodes, materials: cityMaterials } = useGLTF("/models/MovingCity/city.gltf");
 
   const {
     nodes: ferryNodes,
     materials: ferryMaterials,
     animations: ferryAnimations,
   } = useGLTF("/models/MovingCity/ferries.gltf");
-  const { actions } = useAnimations(ferryAnimations, ferryGroup);
-  const { actions: f3actions } = useAnimations(ferryAnimations, ferry3Group);
-  useEffect(() => {
-    actions?.F1Anim.play();
-    f3actions?.F3Anim.play();
-  }, []);
-
-  const cloudGroup = useRef();
 
   const {
     nodes: cloudNodes,
     materials: cloudMaterials,
     animations: cloudAnimations,
   } = useGLTF("/models/MovingCity/clouds.gltf");
+
+  const groupRef = useRef<THREE.Group>(null!);
+  const ferryGroup = useRef();
+  const ferry3Group = useRef();
+  const cloudGroup = useRef();
+
+  const { actions } = useAnimations(ferryAnimations, ferryGroup);
+  const { actions: f3actions } = useAnimations(ferryAnimations, ferry3Group);
   const { actions: cloudactions } = useAnimations(cloudAnimations, cloudGroup);
+
   useEffect(() => {
-    cloudactions?.CloudsAnim.play();
-  }, []);
+    if (actions?.F1Anim) actions.F1Anim.play();
+    if (f3actions?.F3Anim) f3actions.F3Anim.play();
+    if (cloudactions?.CloudsAnim) cloudactions.CloudsAnim.play();
+  }, [actions, f3actions, cloudactions]);
+
+  const materials = useMemo(() => {
+    const clonedMaterials = {
+      ...cityMaterials,
+      ...ferryMaterials,
+      ...cloudMaterials
+    };
+
+    Object.values(clonedMaterials).forEach(material => {
+      if (material instanceof THREE.MeshStandardMaterial) {
+        material.roughness = 1;
+        material.needsUpdate = true;
+      }
+    });
+
+    return clonedMaterials;
+  }, [cityMaterials, ferryMaterials, cloudMaterials]);
 
   return (
     <group>
-      <group {...props} dispose={null} ref={groupRef} position={[0, 0, 0]}>
+      <group {...props} dispose={null} ref={groupRef} position={[0, 0, 0]} rotation={[0, Math.PI / -2, 0]}>
         <mesh
           castShadow={true}
           receiveShadow={true}
-          geometry={nodes.Water.geometry}
+          geometry={cityNodes.Water.geometry}
           material={materials["Material.001"]}
+          frustumCulled={true}
         />
         <group name="Clouds" ref={cloudGroup}>
           <mesh
             name="Water003"
-            castShadow={true}
             receiveShadow={true}
             geometry={cloudNodes.Water003.geometry}
-            material={cloudMaterials["Material.001"]}
-            // ref={cloudGroup}
-            // position={[0, 0, -0.126]}
+            material={materials["Material.001"]}
+            frustumCulled={true}
           />
         </group>
         <group name="Ferry1" ref={ferryGroup}>
           <mesh
             name="Cube007"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube007.geometry}
             material={ferryMaterials.Green}
+            frustumCulled={true}
           />
           <mesh
             name="Cube007_1"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube007_1.geometry}
             material={ferryMaterials.Yellow}
+            frustumCulled={true}
           />
           <mesh
             name="Cube007_2"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube007_2.geometry}
             material={ferryMaterials.Glass}
+            frustumCulled={true}
           />
           <mesh
             name="Cube007_3"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube007_3.geometry}
             material={ferryMaterials.Door}
+            frustumCulled={true}
           />
           <mesh
             name="Cube007_4"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube007_4.geometry}
             material={ferryMaterials.Ramp}
+            frustumCulled={true}
           />
           <mesh
             name="Cube007_5"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube007_5.geometry}
             material={ferryMaterials.Wood}
+            frustumCulled={true}
           />
           <mesh
             name="Cube007_6"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube007_6.geometry}
             material={ferryMaterials.Black}
+            frustumCulled={true}
           />
           <mesh
             name="Cube007_7"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube007_7.geometry}
             material={ferryMaterials.Red}
+            frustumCulled={true}
           />
           <mesh
             name="Cube007_8"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube007_8.geometry}
             material={ferryMaterials["Aircon Grey"]}
+            frustumCulled={true}
           />
           <mesh
             name="Cube007_9"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube007_9.geometry}
             material={ferryMaterials["Aircon Grill"]}
+            frustumCulled={true}
           />
           <mesh
             name="Cube007_10"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube007_10.geometry}
             material={ferryMaterials["Aircon faceplate"]}
+            frustumCulled={true}
           />
           <mesh
             name="Cube007_11"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube007_11.geometry}
             material={ferryMaterials.Liferaft}
+            frustumCulled={true}
           />
         </group>
         <group name="Ferry3" ref={ferry3Group}>
           <mesh
             name="Cube008"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube008.geometry}
             material={ferryMaterials.Green}
+            frustumCulled={true}
           />
           <mesh
             name="Cube008_1"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube008_1.geometry}
             material={ferryMaterials.Yellow}
+            frustumCulled={true}
           />
           <mesh
             name="Cube008_2"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube008_2.geometry}
             material={ferryMaterials.Glass}
+            frustumCulled={true}
           />
           <mesh
             name="Cube008_3"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube008_3.geometry}
             material={ferryMaterials.Door}
+            frustumCulled={true}
           />
           <mesh
             name="Cube008_4"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube008_4.geometry}
             material={ferryMaterials.Ramp}
+            frustumCulled={true}
           />
           <mesh
             name="Cube008_5"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube008_5.geometry}
             material={ferryMaterials.Wood}
+            frustumCulled={true}
           />
           <mesh
             name="Cube008_6"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube008_6.geometry}
             material={ferryMaterials.Black}
+            frustumCulled={true}
           />
           <mesh
             name="Cube008_7"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube008_7.geometry}
             material={ferryMaterials.Red}
+            frustumCulled={true}
           />
           <mesh
             name="Cube008_8"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube008_8.geometry}
             material={ferryMaterials["Aircon Grey"]}
+            frustumCulled={true}
           />
           <mesh
             name="Cube008_9"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube008_9.geometry}
             material={ferryMaterials["Aircon Grill"]}
+            frustumCulled={true}
           />
           <mesh
             name="Cube008_10"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube008_10.geometry}
             material={ferryMaterials["Aircon faceplate"]}
+            frustumCulled={true}
           />
           <mesh
             name="Cube008_11"
-            castShadow={true}
-            receiveShadow={true}
             geometry={ferryNodes.Cube008_11.geometry}
             material={ferryMaterials.Liferaft}
+            frustumCulled={true}
           />
         </group>
       </group>
@@ -262,4 +225,8 @@ export function SydneyModelMoving(props: any) {
   );
 }
 
-// useGLTF.preload("/models/sydney.gltf");
+useGLTF.preload("/models/MovingCity/city.gltf");
+useGLTF.preload("/models/MovingCity/ferries.gltf");
+useGLTF.preload("/models/MovingCity/clouds.gltf");
+
+useGLTF.clear();
--- app/graphics/three-root.tsx
+++ app/graphics/three-root.tsx
@@ -1,33 +1,118 @@
 "use client";
 
-import React from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { useEffect, useState, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 import { SydneyModelMoving } from "@/app/graphics/SydneyModelMoving";
+import * as THREE from 'three';
+import { Loader } from "@react-three/drei";
+import { Suspense } from 'react';
 
 const Lights = () => {
+  const light = useRef<THREE.PointLight>(null!);
+
   return (
     <>
-      <ambientLight intensity={1} />
+      <ambientLight intensity={2} />
       <pointLight
-        intensity={1}
+        ref={light}
+        intensity={4000}
         shadow-bias={-0.002}
-        position={[-5, 10, 20]}
+        position={[10, 20, 10]}
         shadow-mapSize-width={1024}
         shadow-mapSize-height={1024}
         castShadow={true}
       />
+      {/* {light.current && (
+        <primitive object={new THREE.PointLightHelper(light.current, 5)} />
+      )} */}
     </>
   );
 };
 
+const CameraController = () => {
+  const [mouseX, setMouseX] = useState(0);
+  const currentAngle = useRef(Math.PI * 0.5); // Store current angle
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      setMouseX(e.clientX / window.innerWidth - 0.5);
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
+  useFrame((state) => {
+    const radius = 90;
+    const baseAngle = Math.PI * 0.5;
+    const rotationRange = Math.PI * 0.25;
+    const targetAngle = baseAngle + (mouseX * rotationRange);
+
+    // Smooth interpolation
+    currentAngle.current = THREE.MathUtils.lerp(
+      currentAngle.current,
+      targetAngle,
+      0.05 // Adjust this value to change smoothing (0-1, lower = smoother)
+    );
+
+
+    state.camera.position.x = radius * Math.cos(currentAngle.current);
+    state.camera.position.y = 20;
+    state.camera.position.z = radius * Math.sin(currentAngle.current);
+    state.camera.lookAt(0, 20, 0);
+  });
+
+  return null;
+};
+
 export default function ThreeRoot() {
   return (
-    <Canvas
-      shadows={true}
-      camera={{ position: [100, 20, 0], rotation: [0, Math.PI * 0.5, 0] }}
-    >
-      <Lights />
-      <SydneyModelMoving />
-    </Canvas>
+    <>
+      <Canvas
+        shadows={true}
+        camera={{
+          fov: 75,
+          near: 0.1,
+          far: 1000,
+        }}
+        gl={{
+          antialias: true,
+          powerPreference: "high-performance",
+          // alpha: false,
+          stencil: false,
+          depth: true,
+        }}
+      // frameloop="demand"
+      >
+        <Suspense fallback={null}>
+          <CameraController />
+          <Lights />
+          <SydneyModelMoving />
+        </Suspense>
+      </Canvas>
+      <Loader
+        containerStyles={{
+          background: 'transparent',
+          backdropFilter: 'none',
+        }}
+        innerStyles={{
+          backgroundColor: 'transparent',
+          width: '50vw',
+          maxWidth: '400px',
+        }}
+        barStyles={{
+          backgroundColor: '#ffffff',
+          height: '3px',
+        }}
+        dataStyles={{
+          color: '#ffffff',
+          fontSize: '14px',
+          fontFamily: 'monospace',
+          marginTop: '1em',
+        }}
+        dataInterpolation={(p) => `Loading Sydney Harbour ${p.toFixed(0)}%`}
+      />
+    </>
   );
 }
